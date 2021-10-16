f01bfcd59285610d40a3edbc43a421747a181866
Derek Bracken <derekbracken@gmail.com>
2021-10-16 23:52:05 +0100

    update profile feature added

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Dashboard from './Dashboard';
 import Login from './Login';
 import PrivateRoute from './PrivateRoute';
+import ForgotPassword from './ForgotPassword'
+import updateProfile from './UpdateProfile';
 
 function App() {
   return (
@@ -16,8 +18,10 @@ function App() {
           <AuthProvider>
             <Switch>
               <PrivateRoute exact path="/" component={Dashboard} />
+              <PrivateRoute path="/update-profile" component={updateProfile} />
               <Route path="/signup" component={Signup} />
               <Route path="/login" component={Login} />
+              <Route path="/forgot-password" component={ForgotPassword} />
             </Switch>
           </AuthProvider>
         </Router>
