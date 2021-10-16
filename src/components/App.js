2eeb06cb91d7d6784e0dbae5966f503c4b999dc0
Derek Bracken <derekbracken@gmail.com>
2021-10-16 22:58:37 +0100

    login page and dashboard added, 43:30

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,9 @@ import { Container } from 'react-bootstrap';
 import { AuthProvider } from '../contexts/AuthContext';
 import Signup from './Signup';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Dashboard from './Dashboard';
+import Login from './Login';
+import PrivateRoute from './PrivateRoute';
 
 function App() {
   return (
@@ -12,7 +15,9 @@ function App() {
         <Router>
           <AuthProvider>
             <Switch>
+              <PrivateRoute exact path="/" component={Dashboard} />
               <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
             </Switch>
           </AuthProvider>
         </Router>
